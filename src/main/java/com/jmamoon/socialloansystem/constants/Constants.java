21327a3a36980d3eeb2ef31de47b6683612fa8ca
Jose Arandia Luna <jose.arandia.luna@mail.com>
2019-03-04 21:40:17 -0400

    Added constants

---
+++ src/main/java/com/jmamoon/socialloansystem/constants/Constants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jmamoon.socialloansystem.constants;
+
+/**
+ *
+ * @author Jose Arandia Luna https://github.com/jma-moon
+ */
+public class Constants {
+
+    public static final int MONTHS = 12;
+    public static final int LEGAL_ADULT_AGE = 18;
+    public static final int BREAKPOINT_AGE = 30;
+    public static final int MAX_AGE = 60;
+    public static final int YEARS_TO_PAY_YOUNG_PERSON = 30;
+    public static final int YEARS_TO_PAY_OLD_PERSON = 25;
+
+}
